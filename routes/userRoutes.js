a95e78a043d028057a84462043042c3fe4a78d4e
Parth Barai <parthnbarai1234@gmail.com>
2022-10-08 12:04:50 +0530

    razorpay added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,11 @@ const jwtToken = require('../authentication/GetJwtToken')
 const UserSchema = require('../schemas/User')
 const fetchuser = require('../middleware/fetchuser')
 const BookSchema = require('../schemas/Booking')
+const Razorpay = require("razorpay");
+const instance = new Razorpay({
+    key_id: process.env.KEY_ID,
+    key_secret: process.env.KEY_SECRET,
+});
 
 
 //Post route registering user : /api/v1/user/register
@@ -75,7 +80,7 @@ router.post('/book', fetchuser, async (req, res) => {
     try {
         const newBooking = new BookSchema({
             phoneNumber: req.user,
-            busNo : req.body.busNo,
+            busNo: req.body.busNo,
             source: req.body.source,
             destination: req.body.destination,
             depaDate: req.body.depaDate,
@@ -95,8 +100,19 @@ router.post('/book', fetchuser, async (req, res) => {
     }
 })
 
+//Route for booking a seat : /api/v1/user/create/orderId
+router.post("/create/orderId", (req, res) => {
 
-
+    var options = {
+        amount: req.body.amount,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "rcp1"
+    };
+    instance.orders.create(options, function (err, order) {
+        // console.log(order.id);
+        res.send({ orderId: order.id });
+    });
+})
 
 
 
